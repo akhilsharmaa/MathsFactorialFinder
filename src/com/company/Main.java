0d4611a0081b46948fce88451155a9fc2743ab51
akhilsharmaa <akhilsharma.pro@gmail.com>
2020-12-24 21:07:04 +0530

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,22 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        System.out.println( "Facorial is "+ factorial(3));
+
+    }
+
+    public static int factorial (int number){
+
+        int i = 6;
+
+
+
+        return number;
+
+    }
+
+}
